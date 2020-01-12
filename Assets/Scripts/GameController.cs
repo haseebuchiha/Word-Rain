3c4ad615f47206511d641895e299f39990597a7f
ParanoidBat <41900396+ParanoidBat@users.noreply.github.com>
2020-01-13 03:49:23 +0500

    Intial commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public string word;
+
+    private int atIndex = 0;
+
+    private List<GameObject> letterObjects = new List<GameObject>();
+
+    private bool Clicked = false;
+
+    private GameObject objectClicked;
+
+    private SpawnGameObjects spawnGameObjects;
+    private OnClick onClickObject = new OnClick()  ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("inside start");
+        word = word.ToLower();
+
+        FillObjectsList();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // Debug.Log("inside update");
+
+        Clicked = onClickObject.ClickStatus();
+        Debug.Log("value of clicked is sexful      =  " + Clicked);
+        
+        
+        if (Clicked)
+        {
+            // update the statuses
+            onClickObject.SetClickStatus(false);
+            Debug.Log("update main hon mission sexful");
+            Clicked = false;
+
+            //get the object clicked and compare
+            objectClicked = onClickObject.GetObject();
+
+            if (!isCorrect())
+            {
+                Debug.Log("Incorrect!");
+            }
+        }
+    }
+
+    void FillObjectsList() // get the relevant game objects from spawner.
+    {
+        Debug.Log("Fill object method ");
+
+        GameObject go;
+        foreach(char c in word)
+        {
+          //  go = (GameObject)spawnGameObjects.GetGameObject((int)c - 97);
+
+           //  letterObjects.Add(go);
+        }
+    }
+
+    bool isCorrect()
+    {
+        if (objectClicked == letterObjects[atIndex])
+        {
+            atIndex++;
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ Assets/Scripts/OnClick.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnClick : MonoBehaviour
+{
+    private GameObject tapped;
+    public static bool isClicked = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Start method in onclick");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("imside on mouse down");
+        isClicked = true;
+        Debug.Log("is clicked = " + isClicked);
+        
+        // get the object tapped and then destroy
+        tapped = gameObject;
+
+        // Destroy the gameObject after clicking on it
+        Destroy(gameObject);
+    }
+
+    public GameObject GetObject()
+    {
+        return tapped;
+    }
+
+    public bool ClickStatus()
+    {
+        Debug.Log("ClickStatus = " + isClicked);
+        return isClicked;
+    }
+
+    public void SetClickStatus(bool status) // so that game controller can set status false again after receiving status true
+    {
+        isClicked = status;
+    }
+}
