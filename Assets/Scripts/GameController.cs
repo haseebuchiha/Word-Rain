7793c4496c67ebaff972c032cd2a208e69f4743e
ParanoidBat <41900396+ParanoidBat@users.noreply.github.com>
2020-01-18 14:44:18 +0500

    words array iteration

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,46 +1,70 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
-    public string word;
-    private string alphabet;
+    public string[] words;
+    private string alphabet; // the alphabet caught in the game
+    private string currentWord; // the current word displaying in the game
 
+    private int atWord = 0;
     private int atIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        word = word.ToUpper();        
+        currentWord = words[atWord]; // set the current word once
+        currentWord = currentWord.ToUpper();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0)) // if left mouse button is clicked
         {
-            isCorrect();
+           Check();
         }
     }
 
-    void isCorrect(){
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    void Check(){
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); // throw ray from screen at mouse position, into scene
         RaycastHit hit;
 
         if(Physics.Raycast(ray, out hit, Mathf.Infinity)){
             alphabet = hit.transform.name;
-            if(!alphabet.Equals("floor")){
+            
+            if(!alphabet.Equals("floor")){ // destroy any object that was hit, except floor. Don't wanna destroy the floor
                 Destroy(hit.transform.gameObject);
             }
 
-            if(alphabet[0].ToString() == word[atIndex].ToString()){
+            if(alphabet[0].ToString() == currentWord[atIndex].ToString()){ // check if alphabet we clicked is same as alphabet we want
                 Debug.Log("sahi ponche o: " + alphabet);
                 atIndex++;
+
+                if(atIndex == currentWord.Length){ // calls for next word
+                    atIndex = 0;
+                    atWord++;
+                }
+
+                if(atWord == words.Length){
+                    Debug.Log("Game khatam");
+                    SceneManager.LoadScene("MainMenu");
+                }
+                else{
+                    SetNextWord(); // if words have not been completed. Continue with next word
+                }
             }
             else{
                 Debug.Log("nai bhai aese nai: " + alphabet);
+                SceneManager.LoadScene("MainMenu");
             }
         }
+    }
+
+    void SetNextWord(){
+        currentWord = words[atWord];
+        currentWord = currentWord.ToUpper();
     }
 }
